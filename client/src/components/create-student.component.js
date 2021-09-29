db591c3351951c9f676b30c09e5ec8b4ad9dfd9d
parmeshwar <rathodparmeshwar4321@gmail.com>
2021-09-29 16:52:38 +0530

    mern files added

---
+++ client/src/components/create-student.component.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import axios from 'axios'
+
+export default class CreateStudent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        // Setting up functions
+        this.onChangeStudentName = this.onChangeStudentName.bind(this);
+        this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this);
+        this.onChangeStudentRollno = this.onChangeStudentRollno.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            name: "",
+            email: "",
+            rollno: ""
+
+        }
+    }
+
+    onChangeStudentName(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    onChangeStudentEmail(e) {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    onChangeStudentRollno(e) {
+        this.setState({
+            rollno: e.target.value
+        })
+    }
+    onSubmit(e) {
+        e.preventDefault()
+
+        const studentObject = {
+            name: this.state.name,
+            email: this.state.email,
+            rollno: this.state.rollno
+        };
+
+        console.log(`Student successfully created!`);
+        // alert(`Student successfully created!`);
+        alert(process.env.PORT)
+        console.warn(`Name: ${this.state.name}`);
+        console.warn(`Email: ${this.state.email}`);
+        console.warn(`Roll no: ${this.state.rollno}`);
+
+        axios.post('http://localhost:3030/students/create-student', studentObject)
+            .then(res => console.log(res.data));
+
+        this.setState({ name: '', email: '', rollno: '' })
+    }
+
+    render() {
+        return (<div className="form-wrapper">
+            <Form onSubmit={this.onSubmit}>
+
+                <Form.Group controlId="Name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" value={this.state.name} onChange={this.onChangeStudentName} required />
+                </Form.Group>
+
+                <Form.Group controlId="Email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" value={this.state.email} onChange={this.onChangeStudentEmail}   required/>
+                </Form.Group>
+
+                <Form.Group controlId="Name">
+                    <Form.Label>Roll No</Form.Label>
+                    <Form.Control type="text" value={this.state.rollno} onChange={this.onChangeStudentRollno}  required/>
+                </Form.Group>
+            <Button variant="danger" size="lg" blocl="block" type="submit">
+                Create Student
+            </Button>
+            </Form>
+
+        </div>);
+    }
+}
